898f03b742994cc9d4bba21849b3f4bcbb7cec9a
unknown <kevin.wu@ld.com>
2026-06-11 14:18:50 +0800

    Remove --accept-data-loss to prevent dropping other schemas tables

--- backend/start.sh
+++ backend/start.sh
@@ -8,7 +8,7 @@ echo "Generating Prisma client..."
 npx prisma generate
 
 echo "Pushing schema to database..."
-npx prisma db push --skip-generate --accept-data-loss
+npx prisma db push --skip-generate
 
 echo "Seeding admin user..."
 node prisma/seed.js
